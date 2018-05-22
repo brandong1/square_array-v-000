bca93b2d3045585ce68018bb9790ad94dc734355
Brandon Green <brandon@brandonleeproductions.com>
2018-05-22 17:33:45 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
-  square_array.each 
+  square_array.each |array|
 end
 
 # The Google says that to return a NEW array, I'd have to use .map, but that's not part of the requirements so..
